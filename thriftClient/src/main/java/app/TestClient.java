73e5badd3b47419f41f30f8d747cabbf5c953e43
wkkim <wonkkyuu@gmail.com>
2018-04-05 20:37:46 +0900

    thrift client 코드 정리

--- thriftClient/src/main/java/app/TestClient.java
+++ thriftClient/src/main/java/app/TestClient.java
@@ -1,4 +1,5 @@
 package app;
+
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
@@ -9,42 +10,38 @@ import org.slf4j.LoggerFactory;
 
 import thrift.CalculatorService;
 
-public class TestClient
-{
-	Logger logger = LoggerFactory.getLogger(TestClient.class);
+public class TestClient {
 	
-	private void test() throws TException
-	{
+	private static final Logger logger = LoggerFactory.getLogger(TestClient.class);
+
+	private void test() throws TException {
+		
 		TTransport transport = new TSocket("localhost", 8000);
 		TProtocol protocol = new TBinaryProtocol(transport);
-		
+
 		CalculatorService.Client client = new CalculatorService.Client(protocol);
-		
+
 		transport.open();
-		
+
 		int addResult = client.add(5, 5);
-		logger.debug("***** remote add procedure call !!! => result = {}", addResult);
-		
+		logger.debug("***** remote [add] procedure call !!! => result = {}", addResult);
+
 		int subResult = client.subtract(10, 7);
-		logger.debug("***** remote substract procedure call !!! => result = {}", subResult);
-		
+		logger.debug("***** remote [substract] procedure call !!! => result = {}", subResult);
+
 		double divideResult = client.divide(15, 0);
-		logger.debug("***** remote divide procedure call !!! => result = {}", divideResult);
-		
+		logger.debug("***** remote [divide] procedure call !!! => result = {}", divideResult);
+
 		transport.close();
 	}
-	
-	public static void main(String[] args)
-	{
-		try
-		{
+
+	public static void main(String[] args) {
+		
+		try {
 			new TestClient().test();
-		}
-		catch (TException e)
-		{
+		} catch (TException e) {
 			e.printStackTrace();
 		}
 	}
 
-	
 }
