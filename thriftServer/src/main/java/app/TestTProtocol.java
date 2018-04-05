daf515d6ab3f7959a5ef29b1e9b8f0120c1ce64a
wkkim <wonkkyuu@gmail.com>
2018-04-05 20:34:08 +0900

    thrift server 코드 정리

--- thriftServer/src/main/java/app/TestTProtocol.java
+++ thriftServer/src/main/java/app/TestTProtocol.java
@@ -9,34 +9,34 @@ import org.slf4j.LoggerFactory;
 
 import thrift.User;
 
-public class TestTProtocol
-{
-	Logger logger = LoggerFactory.getLogger(TestTProtocol.class);
+public class TestTProtocol {
 	
-	public void test() throws TException
-	{
-		TSerializer binarySerializer = new TSerializer(new TBinaryProtocol.Factory());
+	private static final Logger LOG = LoggerFactory.getLogger(TestTProtocol.class);
+
+	public void test() throws TException {
 		
-		byte[] bytes = binarySerializer.serialize(new User("wkkim"));
+		TSerializer serializer = null;
 		
-		logger.debug("***** bytes = {}", bytes);
-		
-		
-		TSerializer jsonSerializer = new TSerializer(new TSimpleJSONProtocol.Factory());
-		
-		String json = jsonSerializer.toString(new User("wkkim"));
-		
-		logger.debug("***** json = {}", json);
-	}
-	
-	public static void main(String[] args)
-	{
-		try
-		{
-			new TestTProtocol().test();
+		{	/* binary serialize 테스트 */
+			serializer = new TSerializer(new TBinaryProtocol.Factory());
+			
+			byte[] bytes = serializer.serialize(new User("wkkim"));
+			LOG.debug("***** bytes = {}", bytes);
 		}
-		catch (TException e)
-		{
+
+		{	/* json serialize 테스트 */
+			serializer = new TSerializer(new TSimpleJSONProtocol.Factory());
+			
+			String json = serializer.toString(new User("wkkim"));
+			LOG.debug("***** json = {}", json);
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		try {
+			new TestTProtocol().test();
+		} catch (TException e) {
 			e.printStackTrace();
 		}
 	}
--- thriftServer/src/main/java/app/TestTServer.java
+++ thriftServer/src/main/java/app/TestTServer.java
@@ -4,43 +4,41 @@ import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TBinaryProtocol.Factory;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.server.TThreadPoolServer.Args;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TTransportException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import thrift.CalculatorService;
 import thrift.CalculatorServiceImpl;
-import thrift.HelloWorld;
-import thrift.HelloWorldImpl;
 
 public class TestTServer
 {
-	private void test() throws TTransportException
-	{
-		Factory portFactory = new TBinaryProtocol.Factory(true, true);
+	private static final Logger LOG = LoggerFactory.getLogger(TestTServer.class);
+	
+	private void test() throws TTransportException {
 		
 		TServerSocket serverTransport = new TServerSocket(8000);
 		
 		CalculatorService.Processor<CalculatorService.Iface> processor = new CalculatorService.Processor<CalculatorService.Iface>(new CalculatorServiceImpl());
+		Factory portFactory = new TBinaryProtocol.Factory(true, true);
 		
-		org.apache.thrift.server.TThreadPoolServer.Args args = new org.apache.thrift.server.TThreadPoolServer.Args(serverTransport);
+		Args args = new Args(serverTransport);
 		args.processor(processor);
 		args.protocolFactory(portFactory);
 		
 		TServer server = new TThreadPoolServer(args);
-		
-		System.out.println("Starting server on port 8000 ...");
-		
 		server.serve();
+		
+		LOG.debug("*** Starting server on port 8000 ...");
 	}
 	
-	public static void main(String[] args)
-	{
-		try
-		{
+	public static void main(String[] args) {
+		
+		try {
 			new TestTServer().test();
-		}
-		catch (TTransportException e)
-		{
+		} catch (TTransportException e) {
 			e.printStackTrace();
 		}
 	}
